4ec7ea23c52e9ac181ee54c4153c408eb7faea9b
Илья Дуцев <ilya_dutsev@mail.ru>
2020-01-23 01:13:38 +0300

    lesson 99

---
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));    
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) 
+    {
+        var waypoints = waveConfig.GetWaypoints();
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waypoints[0].transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+        
+    }
+}
